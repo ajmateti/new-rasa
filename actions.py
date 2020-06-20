8b05f7320ccf4bb84230b0ec9d43bd44f1303895
ajaybol <ajay.mateti@lji.io>
2020-06-20 23:43:54 +0530

    updated actions and domain

--- actions.py
+++ actions.py
@@ -6,19 +6,6 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 import predict
 
-class ActionHelloWorld(Action):
-
-    def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
-
 
 class ActionAskSymptom(Action):
     def name(self) -> Text:
